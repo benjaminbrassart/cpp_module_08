72909ea0ed01ce677ebecc1aa899367616a689cc
Benjamin BRASSART <bbrassar@student.42.fr>
2022-05-11 11:49:05 +0200

    Renamed easyfind parameter from `container` to `c`

--- ex00/include/easyfind.hpp
+++ ex00/include/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/04 22:20:54 by bbrassar          #+#    #+#             */
-/*   Updated: 2022/05/04 22:27:27 by bbrassar         ###   ########.fr       */
+/*   Updated: 2022/05/11 11:30:12 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@
 # include <algorithm>
 
 template < class Container >
-bool	easyfind(Container const &container, int search_value)
+bool	easyfind(Container const &c, int search_value)
 {
-	return (std::find(container.begin(), container.end(), search_value) != container.end());
+	return (std::find(c.begin(), c.end(), search_value) != c.end());
 }
 
 #endif
