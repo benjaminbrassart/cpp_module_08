d3eeb0e812028b78e428cefa649dbbe56d9b52db
Benjamin BRASSART <bbrassar@student.42.fr>
2022-05-11 11:49:43 +0200

    Improved range generator python script

--- ex01/range.py
+++ ex01/range.py
@@ -3,9 +3,23 @@
 import random
 import sys
 
-if len(sys.argv) != 2:
-	print("Expected 1 argument, %d given" % (len(sys.argv) - 1))
-	exit(1)
+pname = sys.argv[0]
+argv = sys.argv[1:]
+argc = len(argv)
 
-values = random.sample(range(0, 200), int(sys.argv[1]))
-print("\n".join([str(v) for v in values]))
+if 0 < argc < 3:
+    n = int(argv[0])
+    if argc == 2:
+        o = int(argv[1])
+    else:
+        o = n
+    if o < n:
+        print("Error: <max> cannot be less that <range>")
+        exit(1)
+    values = random.sample(range(0, o), n)
+    print("\n".join([str(v) for v in values]))
+    exit(0)
+
+print("Usage: %s <range> <max>" % (pname))
+exit(1)
+
